17f78f70aca3eef47423eec5284be13ede4f292a
Madhavi <madhavi.0108@gmail.com>
2019-12-25 16:27:21 -0800

    changed showNextQuestion function and verifyAnswer function to work only when the button is cicked.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,44 +2,44 @@ var questionEl = document.querySelector(".question");
 var choicesListEl = document.querySelector("#choices-list");
 var answerEl = document.querySelector("#answer");
 var timeSpanEl = document.querySelector("#timer-value");
-var index=0;
+var index = 0;
 //var allDone = false;
 var answer_result;
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-        title: "Which of the following is not considered a JavaScript operator?",
-        choices: ["new", "this", "delete", "typeof"],
-        answer: "this"
-    },
-    {
-        title: " Which of the following can't be done with client-side JavaScript?",
-        choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "None of the above"],
-        answer: "Storing the form's contents to a database file on the server"
-    },
-    {
-        title: " Which of the following attribute can hold the JavaScript version?",
-        choices: ["LANGUAGE", "SCRIPT", "VERSION", "None of the above"],
-        answer: "VERSION"
-    }
-    ///etc.
-  ];
-  const array_length = questions.length;
-  var timer_value = (array_length*15) + 1; //to show the timer value from 75 secs and down--for 5 questions each question can have 15sec each
-  
-  
-  
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Which of the following is not considered a JavaScript operator?",
+    choices: ["new", "this", "delete", "typeof"],
+    answer: "this"
+  },
+  {
+    title: " Which of the following can't be done with client-side JavaScript?",
+    choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "None of the above"],
+    answer: "Storing the form's contents to a database file on the server"
+  },
+  {
+    title: " Which of the following attribute can hold the JavaScript version?",
+    choices: ["LANGUAGE", "SCRIPT", "VERSION", "None of the above"],
+    answer: "VERSION"
+  }
+  ///etc.
+];
+const array_length = questions.length;
+var timer_value = (array_length * 15) + 1; //to show the timer value from 75 secs and down--for 5 questions each question can have 15sec each
 
-  
+
+
+
+
 console.log(questionEl);
 console.log(choicesListEl);
 console.log(answerEl);
@@ -69,96 +69,99 @@ function displayOneQuestions() {
 
 }
 
-  function verifyAnswer(event)
-  {
-    event.preventDefault();
-      var result = false;
-    
-    // on clicking one answer choice button , this function should validate whether the answer is correct ot not and displays it at the bottom of choices. 
-    //then the next question should be displayed along with the answer choices.
+function verifyAnswer(event) {
+  event.preventDefault();
+  var result = false;
 
-    if(event.target.matches("button"))
-    {
-    //  alert("button clicked    --->"+event.target.textContent);
-    //  alert("real answer -->"+questions[index].answer);
-      if(event.target.textContent === questions[index].answer )
-      {
-      //  alert("real answer ~~~~~~~ "+questions[index].answer);
-         console.log("correct");
-         result = true;
-         //answerEl.textContent="answer : correct";
-      } 
-      else
-      {
-        console.log("wrong");
-       // answerEl.textContent="answer : wrong";
-      }
+  // on clicking one answer choice button , this function should validate whether the answer is correct ot not and displays it at the bottom of choices. 
+  //then the next question should be displayed along with the answer choices.
 
-    }
 
-    if(result)
-      answer_result = "correct" ; 
-    else answer_result = "wrong" ;
-// alert("answer_result returnes "+answer_result);
-    return answer_result;
+  // alert("button clicked    --->" + event.target.textContent);
+  //  alert("real answer -->"+questions[index].answer);
+  if (event.target.textContent === questions[index].answer) {
+    //  alert("real answer ~~~~~~~ "+questions[index].answer);
+    console.log("correct");
+    result = true;
+    //answerEl.textContent="answer : correct";
+  }
+  else {
+    console.log("wrong");
+    // answerEl.textContent="answer : wrong";
   }
 
-  function deleteAllChildNodes()
-  {
 
-    var lastChild = choicesListEl.lastElementChild;
-    while(lastChild)
-    {
-      choicesListEl.removeChild(lastChild); 
-			lastChild = choicesListEl.lastElementChild; 
-    }
 
+  if (result)
+    answer_result = "correct";
+  else answer_result = "wrong";
+  // alert("answer_result returnes "+answer_result);
+  return answer_result;
+}
+
+function deleteAllChildNodes() {
+
+  var lastChild = choicesListEl.lastElementChild;
+  while (lastChild) {
+    choicesListEl.removeChild(lastChild);
+    lastChild = choicesListEl.lastElementChild;
   }
+
+}
 
 function showNextQuestion(event) {
 
   event.preventDefault();
+  var correct_or_worng;
+  if (event.target.matches("button")) {
+    // alert("button is clicked ");
+    correct_or_worng = verifyAnswer(event);
 
- // alert("enetered showNextQuestion function ");
-  //alert("index before "+index); 
-  var correct_or_worng = verifyAnswer(event);
-  index++;
- 
-  //alert("Your answer is "+correct_or_worng);
-  if(correct_or_worng === "wrong")
-  {
-    // alert("if");
-    //reduce the timer by 10seconds
-    timer_value-=10;
+
+    // else{
+    //   alert("its not a button ");
+    // }
+
+    //alert("Your answer is "+correct_or_worng);
+    if (correct_or_worng === "wrong") {
+
+      //reduce the timer by 10seconds
+      timer_value -= 10;
+    }
+    index++;
+
+    if (index < array_length)
+      displayOneQuestions();
+    else {
+
+      //storing the Final score in local storage to use it in All done page
+      localStorage.setItem("final_score", timer_value);
+      window.open("./All-Done.html", "_self", false);
+
+    }
+
   }
 
-
-
-  if (index < array_length)
-    displayOneQuestions();
-  else {
-
-        //storing the Final score in local storage to use it in All done page
-        localStorage.setItem("final_score",timer_value);
-        window.open("./All-Done.html","_self",false);
-      
-      }
+  else {// alert("its not a button "); 
+  }
 
 }
 
 
-  //setTimer() sets the timer value to (array length * 15 secs) and counts down till it reaches 0
+//setTimer() sets the timer value to (array length * 15 secs) and counts down till it reaches 0
 
-  function setTimer()
-  {
-    var timerInterval = setInterval(function(){
+function setTimer() {
+  var timerInterval = setInterval(function () {
     timer_value--;
     timeSpanEl.textContent = timer_value;
-    if(timer_value===0)
-    clearInterval(timerInterval);
-    },1000);
-  }
+    if (timer_value === 0)
+      clearInterval(timerInterval);
+  }, 1000);
+}
 
-  setTimer();
-  choicesListEl.addEventListener("click",showNextQuestion);
-  
+setTimer();
+
+//using event delegation to click on list items and to go to the next question in the quiz
+
+choicesListEl.addEventListener("click", showNextQuestion);
+
