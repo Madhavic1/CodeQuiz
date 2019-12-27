5b1813eaa7e30645958a23dfb15074f58bae42f2
Madhavi <madhavi.0108@gmail.com>
2019-12-27 07:41:19 -0800

    added a function compareScores to sort the array of objects ,which is retrieved from localStorage to display highscores in discending order of the scores

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -1,15 +1,54 @@
 var goBackBtnEl = document.querySelector("#go-back");
 var clearHighScoresBtnEl = document.querySelector("#clear-highscores");
+var data = JSON.parse(localStorage.getItem("list"));
+var data_values = [];
 
-console.log("goBackBtnEl "+goBackBtnEl);;
-console.log("clearHighScoresBtnEl "+clearHighScoresBtnEl);
+// console.log("goBackBtnEl "+goBackBtnEl);;
+// console.log("clearHighScoresBtnEl "+clearHighScoresBtnEl);
 
-function displayHighScores()
-{
-    alert(" display High scores");
+function displayHighScores() {
+
+   
+    data_values = Object.values(data);
+
+    for (let i = 0; i < data_values.length; i++) {
+       console.log("data_values[" + i + "] = " + data_values[i].initial + " scored " + data_values[i].finalScore);
+     }
+
+console.log("*****************************************************************");
+
+
+//sorting the array of objects  data_values[] using Array.sort() method using compareFunction parameter
+
+    function compareScores(a, b) {
+        const scoreA = a.finalScore;
+        const scoreB = b.finalScore;
+
+        let comparison = 0;
+        if (scoreA < scoreB) {
+            comparison = 1;
+        }
+
+        else if (scoreA > scoreB) {
+            comparison = -1;
+        }
+
+        return comparison;
+    }
+
+    data_values.sort(compareScores);
+
+
+    for (let i = 0; i < data_values.length; i++) {
+        console.log("data_values[" + i + "] = " + data_values[i].initial + " scored " + data_values[i].finalScore);
+    }
+
 }
 
-displayHighScores();
+
+
+//does something immediately after the page is load
+window.addEventListener("load",displayHighScores);
 
 goBackBtnEl.addEventListener("click",function(event){
     alert(" here ");
