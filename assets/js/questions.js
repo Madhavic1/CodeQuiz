85df5175a50d818514867f0c1961b1fd441dd693
Madhavi <madhavi.0108@gmail.com>
2020-01-03 18:27:07 -0800

    added script for getting the audio elements added in question.html and added code to invoke play() method on those sound elements

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,21 @@
+var quizValue = localStorage.getItem("quizValue");
 var questionEl = document.querySelector(".question");
 var choicesListEl = document.querySelector("#choices-list");
 var answerEl = document.querySelector("#answer");
 var timeSpanEl = document.querySelector("#timer-value");
 
- var correctSoundEl = document.getElementById("sound");
- var correct_or_wrong;
+var correctSoundEl = document.getElementById("correct-sound");
+console.log("correctSoundEl = "+correctSoundEl);
+
+var wrongSoundEl = document.getElementById("wrong-sound");
+console.log("wrongSoundEl = "+wrongSoundEl);
+
+var correct_or_wrong;
 var index = 0;
 //var allDone = false;
 var answer_result;
-var questions = [
+var questions;
+var questions1 = [
   {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
@@ -36,28 +43,86 @@ var questions = [
   }
   ///etc.
 ];
+
+var questions2 = [
+  {
+    title: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+    choices: ["global variable", "local variable", "Both of the above", "None of the above"],
+    answer: "global variable"
+  },
+  {
+    title: "Which of the following is an advantage of using JavaScript?",
+    choices: ["Less Sever Interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above"],
+    answer: "All of the above"
+  },
+  {
+    title: "JavaScript Code can be called by using ",
+    choices: ["RMI", "Triggering Event", "Preprocessor", "Function/Method"],
+    answer: "Function/Method"
+  },
+  {
+    title: "A proper scripting language is a",
+    choices: ["High level programming language", "Assembly level programming language", "Machine level programming language", "Low level programming language"],
+    answer: "High level programming language"
+  },
+  {
+    title: "Which of the following is not considered as an error in JavaScript?",
+    choices: ["Syntax error", "Missing of semicolons", "Division by zero", "All of the mentioned"],
+    answer: "Function/Method"
+  }
+];
+
+var questions3 = [
+  {
+    title: "A function with no return value is called",
+    choices: ["Procedures", "Method", "Static function", "Dynamic function"],
+    answer: "Procedures"
+  },
+  {
+    title: "The unordered collection of properties, each of which has a name and a value is called",
+    choices: ["String", "Object", "Serialized Object", "All of the mentioned"],
+    answer: "Object"
+  },
+  {
+    title: "JavaScript can be written",
+    choices: ["directly into JS file and included into HTML", "directly on the server page", "directly into HTML pages", "All of the mentioned"],
+    answer: "directly into JS file and included into HTML"
+  },
+  {
+    title: "The escape sequence ‘\f’ stands for",
+    choices: ["Floating numbers", "Representation of functions that returns a value", "\f is not present in JavaScript", "Form feed"],
+    answer: "Form feed"
+  },
+  {
+    title: "JavaScript is ______ Side Scripting Language.",
+    choices: ["ISP", "Server", "None of these", "Browser"],
+    answer: "Browser"
+  }
+];
+
+if (quizValue == "q1") {
+  questions = questions1;
+}
+else if (quizValue == "q2") {
+  questions = questions2;
+}
+else
+  questions = questions3;
+
 const array_length = questions.length;
 var timer_value = (array_length * 15) + 1; //to show the timer value from 75 secs and down--for 5 questions each question can have 15sec each
 
 
 
 
-
-console.log(questionEl);
-console.log(choicesListEl);
-console.log(answerEl);
-
-
 function displayOneQuestions() {
-
-  //add code to start timer = length of questions array * 15 seconds
 
   var a_length = array_length;
   var c_length = 0;
   var title = questions[index].title;
 
   questionEl.textContent = title;
-// alert("in displayQuestion function "+correct_or_wrong);
+
   if (index > 0) {
     deleteAllChildNodes();
   }
@@ -65,7 +130,7 @@ function displayOneQuestions() {
   for (let j = 0; j < c_length; j++) {
     var li = document.createElement("li");
     li.innerHTML = "<button class= 'choiceBtn'>" + questions[index].choices[j] + "</button>";
-    // console.log(questions[i].choices[j]);
+
     choicesListEl.appendChild(li);
   }
 
@@ -75,27 +140,23 @@ function displayOneQuestions() {
 function verifyAnswer(event) {
   event.preventDefault();
   var result = false;
-//  alert("verifyAnswer");
-  // on clicking one answer choice button , this function should validate whether the answer is correct ot not and displays it at the bottom of choices. 
-  //then the next question should be displayed along with the answer choices.
-  console.log("correct");
+
+  // on clicking one answer choice button , this function should validate whether the answer is correct ot not .
+  console.log("verifyAnswer");
   if (event.target.textContent === questions[index].answer) {
-     console.log("correct");
-      // alert("correct");
-    
+    console.log("correct");
+    // alert("correct");
+
     correctSoundEl.play();
 
     result = true;
   }
   else {
- 
-   // wrongSoundEl.play();
-  //  correctAudioEl.play();
-  correctSoundEl.play();
-   result = false;
+
+    wrongSoundEl.play();
+    result = false;
     console.log("wrong");
-        // alert("wrong");
-    // answerEl.textContent="answer : wrong";
+
   }
 
 
@@ -103,7 +164,7 @@ function verifyAnswer(event) {
   if (result)
     answer_result = "correct";
   else answer_result = "wrong";
-  // alert("answer_result returnes "+answer_result);
+
   return answer_result;
 }
 
@@ -117,20 +178,17 @@ function deleteAllChildNodes() {
 
 }
 
-function displayTheResult()
-{
-  answerEl.textContent="answer : "+correct_or_wrong;
-  setTimeout(function(){answerEl.textContent = "";},500);
+function displayTheResult() {
+  answerEl.textContent = "answer : " + correct_or_wrong;
+  setTimeout(function () { answerEl.textContent = ""; }, 500);
 }
 
 function showNextQuestion(event) {
-// alert("entered showNextQuestion");
+  // alert("entered showNextQuestion");
   event.preventDefault();
 
   if (event.target.matches("button")) {
-    //  alert("button is clicked ");
     correct_or_wrong = verifyAnswer(event);
-    // alert("correct_or_wrong == "+correct_or_wrong);
     if (correct_or_wrong === "wrong") {
 
       //reduce the timer by 15seconds
@@ -141,12 +199,12 @@ function showNextQuestion(event) {
       else {
         // alert("timer_value  in else" + timer_value);
         //timer_value = 0;
-       endTheQuiz();
+        endTheQuiz();
       }
     }
     index++;
 
-    if (index < array_length){
+    if (index < array_length) {
       displayOneQuestions();
       displayTheResult();
     }
@@ -167,8 +225,7 @@ function showNextQuestion(event) {
 }
 
 //The below function sets the timer to zero and redirects the page to All-Done.html 
-function endTheQuiz()
-{
+function endTheQuiz() {
   // correctAudioEl.play();
   // alert("in endTheQuiz ");
   localStorage.setItem("final_score", timer_value);
@@ -184,12 +241,12 @@ function setTimer() {
   var timerInterval = setInterval(function () {
     timer_value--;
     timeSpanEl.textContent = timer_value;
-    if (timer_value === 0){
+    if (timer_value === 0) {
       // alert("timer = 0 ");
       clearInterval(timerInterval);
       window.open("./All-Done.html", "_self", false);
     }
-      
+
   }, 1000);
 }
 
